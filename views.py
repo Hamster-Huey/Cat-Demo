1814d0f1fc0f7fdb921b568d8b69706b72fc337b
unknown <jamchri@MICROSO-VV7BHUM.redmond.corp.microsoft.com>
2017-06-13 16:16:58 -0700

    hi

---
+++ views.py
@@ -0,0 +1,21 @@
+from app import app
+import os
+from flask import send_from_directory
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return send_from_directory('index', 'index.html')
+  
+@app.route('/hi')
+def hi():
+  try: lst = os.listdir('/tmp/test1/app/images')
+  except OSError as err:
+    return str(err)
+  else:
+    return str(len(lst))
+  return '0'
+  
+@app.route('/images/<path:path>')
+def images(path):
+  return send_from_directory('images', path)
